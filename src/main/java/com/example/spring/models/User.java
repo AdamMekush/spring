40817cf3d82d383d5422568443227e5ecadb7579
Adam Mekush <60749996+AdamMekush@users.noreply.github.com>
2022-07-10 12:59:37 +0200

    Create User.java

---
+++ src/main/java/com/example/spring/models/User.java
@@ -0,0 +1,46 @@
+package com.example.spring.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User(){}
+    public User(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+}
