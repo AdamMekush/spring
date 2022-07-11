78d1286c101334b52f5c92f8f6a5a12a29aa6b89
Adam Mekush <60749996+AdamMekush@users.noreply.github.com>
2022-07-11 14:17:22 +0200

    Create UserRepository.java

---
+++ src/main/java/com/example/spring/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.spring.repository;
+
+import com.example.spring.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
